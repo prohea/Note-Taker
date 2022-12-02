0d4a04cdd2f0e95d5d5ce6454da1dc26ee044e52
Mizuri <pro.1.h@outlook.com>
2022-12-01 22:05:17 -0700

    started from starter code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -117,7 +117,7 @@ const handleRenderSaveBtn = () => {
 };
 
 // Render the list of note titles
-async function renderNoteList(notes) {
+const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
@@ -168,7 +168,7 @@ async function renderNoteList(notes) {
   if (window.location.pathname === '/notes') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
-}
+};
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-//Import our modular routers
-const notesRouter = require("./notes");
-const app = express();
-
-app.use("/notes", notesRouter);
-
-module.exports = app;
