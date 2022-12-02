cc2711b62a6f18411d1802f99416f03713d223e7
Mizuri <pro.1.h@outlook.com>
2022-12-01 20:05:24 -0700

    updated serverjs

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ app.get("/notes", (req,res) => res.sendFile(path.join(__dirname, "/public/notes.
 
 //Wildcard route to direct users to a 404 page
 app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/")))
+    res.sendFile(path.join(__dirname, "/public/pages/404.html")));
+    
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
