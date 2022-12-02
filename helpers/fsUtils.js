fb81dbcd964cc062f88bd2afc0beb5d47b809eda
Mizuri <pro.1.h@outlook.com>
2022-12-01 19:20:05 -0700

    added export

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -4,15 +4,15 @@ const util = require("util");
 const readFromFile = util.promisify(fs.readFile);
 
 const readAndAppend = (content, file) => {
-    fs.readFile(file, "utf8", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parsedData;
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-        }
-    });
+	fs.readFile(file, "utf8", (err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			const parsedData = JSON.parsedData;
+			parsedData.push(content);
+			writeToFile(file, parsedData);
+		}
+	});
 };
 
 module.exports = { readFromFile, writeToFile, readAndAppend };
--- routes/notes.js
+++ routes/notes.js
@@ -1,49 +1,52 @@
 const notes = require("express").Router();
 const uuid = require("../helpers/uuid");
 const {
-    readAndAppend,
-    readFromFile,
-    writeToFile
+	readAndAppend,
+	readFromFile,
+	writeToFile,
 } = require("../helpers/fsUtils");
 
 //GET route for getting all the notes
-notes.get("/", (req,res) => {
-    readFromFile("../db/db.json").then((data) => res.json(JSON.parse(data)))
+notes.get("/", (req, res) => {
+	readFromFile("../db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 //POST route for posting notes
-notes.post("/", (req,res) => {
-    const { title, text } = req.body;
+notes.post("/", (req, res) => {
+	const { title, text } = req.body;
 
-    if (title && text) {
-        const newNotes = {
-            title,
-            text,
-            note_id: uuid()
-        };
+	if (title && text) {
+		const newNotes = {
+			title,
+			text,
+			note_id: uuid(),
+		};
 
-        readAndAppend(newNotes, "../db/db.json");
+		readAndAppend(newNotes, "../db/db.json");
 
-        const response = {
-            status: "success",
-            body: newNotes
-        };
+		const response = {
+			status: "success",
+			body: newNotes,
+		};
 
-        res.json(response);
-    } else {
-        res.json("Error in posting notes");
-    }
+		res.json(response);
+	} else {
+		res.json("Error in posting notes");
+	}
 });
 
 //DELETE route for deleting notes
-notes.delete("/:note_id", (req,res) => {
-    const noteId = req.params.note_id;
-    readFromFile("../db/db.json").then((data) => JSON.parse(data)).then((json) => {
-        console.log(json);
-        const result = json.filter((note) => note.note_id !== noteId);
-        
-        writeToFile("../db/db.json", result);
+notes.delete("/:note_id", (req, res) => {
+	const noteId = req.params.note_id;
+	readFromFile("../db/db.json")
+		.then((data) => JSON.parse(data))
+		.then((json) => {
+			console.log(json);
+			const result = json.filter((note) => note.note_id !== noteId);
 
-        res.json(`Item ${noteId} has been deleted`);
-    });
+			writeToFile("../db/db.json", result);
+
+			res.json(`Item ${noteId} has been deleted`);
+		});
 });
 
+module.exports = notes;
