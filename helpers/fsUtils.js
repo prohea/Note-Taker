4045fe93d5f7826ce50ee16d2f44d3693f5db68b
Mizuri <pro.1.h@outlook.com>
2022-12-01 19:04:39 -0700

    added fs utils

---
+++ helpers/fsUtils.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFromFile = util.promisify(fs.readFile);
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parsedData;
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
