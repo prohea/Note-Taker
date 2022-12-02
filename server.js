95baa23ba7ac13377dfbe516fe72c8d83ef9521c
Mizuri <pro.1.h@outlook.com>
2022-12-01 20:07:18 -0700

    updated code

--- server.js
+++ server.js
@@ -7,20 +7,25 @@ const PORT = process.env.PORT || 3001;
 
 //Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use(express.static("public"));
 
 //GET Route for homepage
-app.get("/", (req,res) => 
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/", (req, res) =>
+	res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 //GET Route for note page
-app.get("/notes", (req,res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+app.get("/notes", (req, res) =>
+	res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 //Wildcard route to direct users to a 404 page
 app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/pages/404.html")));
-    
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+	res.sendFile(path.join(__dirname, "/public/pages/404.html"))
+);
+
+app.listen(PORT, () =>
+	console.log(`App listening at http://localhost:${PORT}`)
+);
